bb320d663b332b81eb2be296d6e9cadaa8e8d1b7
Zack Stangroom <st10372065@vcconnect.edu.za>
2024-04-06 10:32:03 +0200

    Add project files.

---
+++ Class/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221POEPart1.Class
+{
+    internal class Class1
+    {
+        public static void Ingredients()
+        {
+
+        }
+    }
+}

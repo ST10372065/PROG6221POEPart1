32e195c6a7d7804c7669a1da33adbed5c855d55f
Zack Stangroom <st10372065@vcconnect.edu.za>
2024-04-06 10:54:42 +0200

    Update 1. added options to do requied things with recipe

--- Class/Class1.cs
+++ Class/Class1.cs
@@ -10,6 +10,20 @@ namespace PROG6221POEPart1.Class
     {
         public static void Ingredients()
         {
+            Console.WriteLine("Please enter the amount of ingredients");
+            String numIngr = Console.ReadLine();
+
+            int numIngredients;
+
+            if (int.TryParse(numIngr, out numIngredients)) //convert string to int
+            {
+                string numberAsString = numIngredients.ToString(); // Convert int to string
+                Console.WriteLine($"You entered: {numberAsString}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+            }
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PROG6221POEPart1.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,19 @@ namespace PROG6221POEPart1
     {
         static void Main(string[] args)
         {
+            PROG6221POEPart1.Class.Class1 cls1 = new Class.Class1();
+            Class1.Ingredients();
+            bool conLoop = true;
 
+            while(conLoop)
+            {
+                Console.WriteLine("1. Enter recipe details");
+                Console.WriteLine("2. Display recipe");
+                Console.WriteLine("3. Scale recipe");
+                Console.WriteLine("4. Reset recipe quantities");
+                Console.WriteLine("5. Clear recipe data");
+                Console.WriteLine("6. Exit");
+            }
         }
     }
 }
